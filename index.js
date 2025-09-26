0860300043a2468fdfd8c9f0c8e9df11c037dc5b
Happiness Ngeete <ndolohappiness@gmail.com>
2025-09-26 14:43:47 +0300

    Solution: DOMContentLoaded event listener

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Step A: find the paragraph by its ID
   const paragraph = document.getElementById("text");
-
-  // Step B: change its text
   paragraph.textContent = "This is really cool!";
 });
