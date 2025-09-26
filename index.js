9db9a4bd853d77a677edec23f346627510dfceb6
Happiness Ngeete <ndolohappiness@gmail.com>
2025-09-26 14:36:42 +0300

    Add DOMContentLoaded event listener solution

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  // Step A: find the paragraph by its ID
+  const paragraph = document.getElementById("text");
+
+  // Step B: change its text
+  paragraph.textContent = "This is really cool!";
+});
